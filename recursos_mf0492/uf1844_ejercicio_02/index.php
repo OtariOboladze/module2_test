254e19313773815c6f4f0dbd1435cf9b8e209d8c
OtariOboladze <otari.oboladze@outlook.com>
2021-03-24 19:18:45 +0100

    ex 3 done

--- recursos_mf0492/uf1844_ejercicio_02/index.php
+++ recursos_mf0492/uf1844_ejercicio_02/index.php
@@ -1,28 +1,51 @@
 <?php
-	//definir una variable con la nota
-	
-	//llamar a la función de obtención de calificación y recoger el resultado en una variable
-	
-	//definir la función 
-	
+$nota = rand(0, 10);
+$calificacion = null;
+
+function test($n){
+	if ($n < 5) {
+		$c = 'Suspenso';
+	} else if ($n == 6 || $n == 7) {
+		$c = 'Aprobado';
+	} else if ($n == 8 || $n == 9) {
+		$c = 'Notable';
+	} else {
+		$c = 'Excelente';
+	}
+	return $c;
+}
+
+$calificacion = test($nota);
+
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Funciones</title>
-	<meta charset='UTF-8'>	
+	<meta charset='UTF-8'>
 	<style type="text/css">
-		div {text-align: center; border:1px solid grey;width: 390px;margin: auto;}
-		h2 {text-align: center;}
+		div {
+			text-align: center;
+			border: 1px solid grey;
+			width: 390px;
+			margin: auto;
+		}
+
+		h2 {
+			text-align: center;
+		}
 	</style>
 </head>
+
 <body>
 	<h2>Funciones</h2>
 	<div>
-		<span>Nota: </span>
+		<span>Nota: <?=$nota?></span>
 	</div>
 	<div>
-		<span>Calificación: </span>
+		<span>Calificación: <?=$calificacion?></span>
 	</div>
 </body>
+
 </html>
