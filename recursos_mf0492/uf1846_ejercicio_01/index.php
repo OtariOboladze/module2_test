d770b4d052a5163eae3ddc722166feea63ff52c2
OtariOboladze <otari.oboladze@outlook.com>
2021-03-25 04:25:47 +0100

    init ex oop

--- recursos_mf0492/uf1846_ejercicio_01/index.php
+++ recursos_mf0492/uf1846_ejercicio_01/index.php
@@ -1,14 +1,80 @@
 <?php
 	//clase genérica Usuario
 
+	class Usuario{
+		
 		//atributos
+		protected $nif;
+		protected $nombre;
+		protected $apellido;
+		protected $direccion;
 
+		
 		//constructor
 
+		public function Usuario($nif, $nom, $ape, $dir ){
+			try {
+				$this->setNif($nif);
+				$this->setNombre($nom);
+				$this->setApellido($ape);
+				$this->setDireccion($dir);
+			} catch (Exception $e) {
+				throw new Exception($e->getMessage());
+			}
+		}
+		
 		//getters y setters
+		public function getNif() {
+			return $this->nif;
+		}
+		public function getNombre() {
+			return $this->nombre;
+		}
+		public function getApellido() {
+			return $this->apellido;
+		}
+		public function getDireccion() {
+			return $this->direccion;
+		}
+
+		public function setNif($nif) {
+			try {
+				if (empty($n)) {
+					throw new Exception("Nombre no informado");
+				}
+
+				$this->nombre = $n;
+			} catch (Exception $e) {
+				throw new Exception($e->getMessage());
+			}
+		}
+		public function setNombre($nom) {
+			if (empty($a)) {
+				echo 'apellido no informado';
+				return;
+			}
+			$this->apellido = $a;
+		}
+		public function setApellido($ape) {
+			if (empty($a)) {
+				echo 'apellido no informado';
+				return;
+			}
+			$this->apellido = $a;
+		}
+		public function setDireccion($dir) {
+			if (empty($a)) {
+				echo 'apellido no informado';
+				return;
+			}
+			$this->apellido = $a;
+		}
+
+
 
 		//otros métodos
-
+		
+	}
 	// ------------------------------------------
 
 	//subclase Alumno
@@ -24,3 +90,16 @@
 	// ------------------------------------------
 
 	//instanciar un Alumno 
+
+
+
+ Los correspondientes getters y setters
+ Un método para mostrar los datos del usuario
+ No podremos instanciar directamente un objeto de esta clase
+
+Necesitamos una clase subordinada Alumno con las siguientes características:
+ Atributo curs
+ Un constructor para informar este nuevo atributo y los de la superclase
+ El correspondiente getter y setter
+ Un método que amplie el método de mostrar datos de la superclase
+ Instanciar un objeto de esta clase y mostrar sus datos
