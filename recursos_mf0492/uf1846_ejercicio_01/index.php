0829c1e24e4bed96c4006562a8bb41419a1f77fd
OtariOboladze <otari.oboladze@outlook.com>
2021-03-25 05:06:32 +0100

    oop done

--- recursos_mf0492/uf1846_ejercicio_01/index.php
+++ recursos_mf0492/uf1846_ejercicio_01/index.php
@@ -1,105 +1,124 @@
 <?php
-	//clase genérica Usuario
+//clase genérica Usuario
 
-	class Usuario{
-		
-		//atributos
-		protected $nif;
-		protected $nombre;
-		protected $apellido;
-		protected $direccion;
+class Usuario
+{
+	//atributos
+	protected $nif;
+	protected $nombre;
+	protected $apellido;
+	protected $direccion;
 
-		
-		//constructor
-
-		public function Usuario($nif, $nom, $ape, $dir ){
-			try {
-				$this->setNif($nif);
-				$this->setNombre($nom);
-				$this->setApellido($ape);
-				$this->setDireccion($dir);
-			} catch (Exception $e) {
-				throw new Exception($e->getMessage());
-			}
-		}
-		
-		//getters y setters
-		public function getNif() {
-			return $this->nif;
-		}
-		public function getNombre() {
-			return $this->nombre;
-		}
-		public function getApellido() {
-			return $this->apellido;
-		}
-		public function getDireccion() {
-			return $this->direccion;
-		}
-
-		public function setNif($nif) {
-			try {
-				if (empty($n)) {
-					throw new Exception("Nombre no informado");
-				}
-
-				$this->nombre = $n;
-			} catch (Exception $e) {
-				throw new Exception($e->getMessage());
-			}
-		}
-		public function setNombre($nom) {
-			if (empty($a)) {
-				echo 'apellido no informado';
-				return;
-			}
-			$this->apellido = $a;
-		}
-		public function setApellido($ape) {
-			if (empty($a)) {
-				echo 'apellido no informado';
-				return;
-			}
-			$this->apellido = $a;
-		}
-		public function setDireccion($dir) {
-			if (empty($a)) {
-				echo 'apellido no informado';
-				return;
-			}
-			$this->apellido = $a;
-		}
-
-
-
-		//otros métodos
-		
+	//constructor
+	public function Usuario($nif, $nom, $ape, $dir)
+	{
+		$this->setNif($nif);
+		$this->setNombre($nom);
+		$this->setApellido($ape);
+		$this->setDireccion($dir);
 	}
-	// ------------------------------------------
 
-	//subclase Alumno
+	//getters y setters
+	public function getNif()
+	{
+		return $this->nif;
+	}
+	public function getNombre()
+	{
+		return $this->nombre;
+	}
+	public function getApellido()
+	{
+		return $this->apellido;
+	}
+	public function getDireccion()
+	{
+		return $this->direccion;
+	}
 
-		//atributo específico
+	public function setNif($nif)
+	{
+		try {
+			if (empty($nif)) {
+				throw new Exception("Nif no informado");
+			}
 
-		//constructor
+			$this->nif = $nif;
+		} catch (Exception $e) {
+			throw new Exception($e->getMessage());
+		}
+	}
+	public function setNombre($nom)
+	{
+		if (empty($nom)) {
+			echo 'nombre no informado';
+			return;
+		}
+		$this->nombre = $nom;
+	}
+	public function setApellido($ape)
+	{
+		if (empty($ape)) {
+			echo 'apellido no informado';
+			return;
+		}
+		$this->apellido = $ape;
+	}
+	public function setDireccion($dir)
+	{
+		if (empty($dir)) {
+			echo 'direccion no informado';
+			return;
+		}
+		$this->direccion = $dir;
+	}
 
-		//getter y setter
+	//otros métodos
 
-		//otros métodos
+	public function info()
+	{
+		echo '<br>Usuarion con nif: ' . $this->nif . '; Nombre: ' . $this->nombre . '; Apellido: ' . $this->apellido . '; Direccion: ' . $this->direccion . '<br>';
+	}
+}
+// ------------------------------------------
 
-	// ------------------------------------------
+//subclase Alumno
+class Alumno extends Usuario
+{
+	//atributo específico
+	private $curso;
 
-	//instanciar un Alumno 
+	//constructor
+	public function Alumno($nif, $nom, $ape, $dir, $cur)
+	{
+		parent::Usuario($nif, $nom, $ape, $dir);
+		$this->setCurso($cur);
+	}
+	//getter y setter
+	public function getCurso()
+	{
+		return $this->curso;
+	}
+	public function setCurso($cur)
+	{
+		$this->curso = $cur;
+	}
 
+	//otros métodos
+	public function info()
+	{
+		echo '<br>Usuarion con nif: ' . $this->nif . '; Nombre: ' . $this->nombre . '; Apellido: ' . $this->apellido . '; Direccion: ' . $this->direccion . ' esta en curso: ' . $this->curso . '<br>';
+	}
+}
+// ------------------------------------------
 
+//instanciar un Alumno 
 
- Los correspondientes getters y setters
- Un método para mostrar los datos del usuario
- No podremos instanciar directamente un objeto de esta clase
+try {
+	$estudiante1 = new Alumno('A1234567B', 'Barac', 'Obama', 'USA', 'Full stack');
+} catch (Exception $e) {
+	//capturar y tratar la excepción
+	echo $e->getMessage();
+}
 
-Necesitamos una clase subordinada Alumno con las siguientes características:
- Atributo curs
- Un constructor para informar este nuevo atributo y los de la superclase
- El correspondiente getter y setter
- Un método que amplie el método de mostrar datos de la superclase
- Instanciar un objeto de esta clase y mostrar sus datos
+echo $estudiante1->info();
